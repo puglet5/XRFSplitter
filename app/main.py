d6f763c6e27e3bef51416dca02a714c2022eeb26
Michael Basmanov <puglet5@mail.ru>
2023-12-13 16:42:22 +0300

    dev: move ui

--- app/main.py
+++ app/main.py
@@ -1,20 +1,4 @@
-import tkinter
-
-
-class UserInterface:
-    def __init__(self):
-        self.app = tkinter.Tk()
-        self.app.title("BTLab Camera Controller")
-        self.app.protocol("WM_DELETE_WINDOW", self.quit)
-        self.app.bind("<Control-q>", lambda _: self.quit())
-        self.app.geometry("1280x720")
-
-    def start(self):
-        self.app.mainloop()
-
-    def quit(self):
-        self.app.quit()
-
+from app.ui import UserInterface
 
 if __name__ == "__main__":
     main = UserInterface()
---
+++ app/ui.py
@@ -0,0 +1,16 @@
+import tkinter
+
+
+class UserInterface:
+    def __init__(self):
+        self.app = tkinter.Tk()
+        self.app.title("BTLab Camera Controller")
+        self.app.protocol("WM_DELETE_WINDOW", self.quit)
+        self.app.bind("<Control-q>", lambda _: self.quit())
+        self.app.geometry("1280x720")
+
+    def start(self):
+        self.app.mainloop()
+
+    def quit(self):
+        self.app.quit()
